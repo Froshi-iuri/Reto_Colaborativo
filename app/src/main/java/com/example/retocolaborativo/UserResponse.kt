e99b907a82240c428ab187673de14cbdee0416c7
deviperez29 <vidaldeivi7@gmail.com>
2026-08-01 11:02:46 -0500

    feat: fill UserResponse with email, firstname and lastName

--- app/src/main/java/com/example/retocolaborativo/UserResponse.kt
+++ app/src/main/java/com/example/retocolaborativo/UserResponse.kt
@@ -3,4 +3,7 @@ package com.example.retocolaborativo
 data class UserResponse(
     val id: Int,
     val username: String,
+    val email: String,
+    val firstName: String,
+    val lastName: String
 )
