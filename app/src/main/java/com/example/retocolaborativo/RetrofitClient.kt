d213114726b50610b1ede842108c5d285bed7dc0
Jose Jose <eope820529@gmail.com>
2026-08-01 11:40:09 -0500

    Se agrega private val y private const val

--- app/src/main/java/com/example/retocolaborativo/RetrofitClient.kt
+++ app/src/main/java/com/example/retocolaborativo/RetrofitClient.kt
@@ -1,4 +1,12 @@
 package com.example.retocolaborativo
 
+import okhttp3.logging.HttpLoggingInterceptor
+
 object RetrofitClient {
+    private const val BASE_URL = "https://dummyjson.com/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+
+    }
 }
