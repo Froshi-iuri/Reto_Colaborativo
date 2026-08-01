e6721a6d7cd67e4456d4bb3761f8740e4a3fccbd
deviperez29 <vidaldeivi7@gmail.com>
2026-08-01 10:57:19 -0500

    feat: fill LoginResponse with fisrtsname, acccessToken, refreshToken

--- app/src/main/java/com/example/retocolaborativo/LoginResponse.kt
+++ app/src/main/java/com/example/retocolaborativo/LoginResponse.kt
@@ -4,4 +4,7 @@ data class LoginResponse(
     val id: Int,
     val username: String,
     val email: String,
+    val firstName: String,
+    val accessToken: String,   // ← el token vive aquí
+    val refreshToken: String
 )
