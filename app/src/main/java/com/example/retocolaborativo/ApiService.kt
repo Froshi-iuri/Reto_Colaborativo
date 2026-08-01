5ecc19459b4c4bbf8c43ac0f4deea7d90d0499c9
Jose Jose <eope820529@gmail.com>
2026-08-01 11:22:43 -0500

    Se añade post login Response

--- app/src/main/java/com/example/retocolaborativo/ApiService.kt
+++ app/src/main/java/com/example/retocolaborativo/ApiService.kt
@@ -1,4 +1,10 @@
 package com.example.retocolaborativo
 
+import okhttp3.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface ApiService {
+    @POST("auth/login")
+    suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
 }
